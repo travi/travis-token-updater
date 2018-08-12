bd65cede02dd213aa3a48767efc4dcfa04d0c5e3
Matt Travi <programmer@travi.org>
2018-08-12 00:53:22 -0500

    wip(orgs): added an orgs fetcher

---
+++ test/unit/organizations-test.js
@@ -0,0 +1,14 @@
+import sinon from 'sinon';
+import {assert} from 'chai';
+import any from '@travi/any';
+import {getList} from '../../src/organizations';
+
+suite('fetch organizations', () => {
+  test('that the list of orgs is fetched for the authenticated session', async () => {
+    const getOrgs = sinon.stub();
+    const organizations = any.listOf(any.simpleObject);
+    getOrgs.resolves({data: organizations});
+
+    assert.equal(await getList({users: {getOrgs}}), organizations);
+  });
+});
