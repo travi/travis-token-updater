24d47f9d5ce70a6fa68a71ef529748b03cdf26ec
Matt Travi <programmer@travi.org>
2019-02-11 23:42:09 -0600

    fix(repos): requested user vs org repos correctly

--- src/account/repos.js
+++ src/account/repos.js
@@ -1,5 +1,13 @@
+import {getDetails as getUserDetails} from './user';
+
+async function accountTypeIsUser(account, octokit) {
+  return account === (await getUserDetails(octokit)).login;
+}
+
 export async function listNames(octokit, account) {
-  const options = octokit.repos.listForUser.endpoint.merge({username: account});
+  const options = await accountTypeIsUser(account, octokit)
+    ? octokit.repos.list.endpoint.merge({})
+    : octokit.repos.listForOrg.endpoint.merge({org: account});
   const repos = await octokit.paginate(options);
 
   return repos.data.map(repo => repo.name);
--- test/unit/account/repos-test.js
+++ test/unit/account/repos-test.js
@@ -2,19 +2,42 @@ import sinon from 'sinon';
 import {assert} from 'chai';
 import any from '@travi/any';
 import {listNames} from '../../../src/account/repos';
+import * as userFetcher from '../../../src/account/user';
 
 suite('account repos', () => {
-  test('that the repo names for the account are listed', async () => {
-    const account = any.word();
-    const names = any.listOf(any.string);
-    const repos = names.map(name => ({...any.simpleObject(), name}));
-    const requestOptions = any.simpleObject();
+  let sandbox;
+  const account = any.word();
+  const repoNames = any.listOf(any.string);
+  const repos = repoNames.map(name => ({...any.simpleObject(), name}));
+  const requestOptions = any.simpleObject();
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(userFetcher, 'getDetails');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that the repo names for the user account are listed', async () => {
     const merge = sinon.stub();
     const paginate = sinon.stub();
-    const client = {...any.simpleObject(), paginate, repos: {...any.simpleObject(), listForUser: {endpoint: {merge}}}};
-    merge.withArgs({username: account}).returns(requestOptions);
+    const client = {...any.simpleObject(), paginate, repos: {...any.simpleObject(), list: {endpoint: {merge}}}};
+    userFetcher.getDetails.withArgs(client).resolves({login: account});
+    merge.withArgs({}).returns(requestOptions);
     paginate.withArgs(requestOptions).resolves({data: repos});
 
-    assert.deepEqual(await listNames(client, account), names);
+    assert.deepEqual(await listNames(client, account), repoNames);
+  });
+
+  test('that the repo names for the user account are listed', async () => {
+    const merge = sinon.stub();
+    const paginate = sinon.stub();
+    const client = {...any.simpleObject(), paginate, repos: {...any.simpleObject(), listForOrg: {endpoint: {merge}}}};
+    userFetcher.getDetails.withArgs(client).resolves({login: any.word()});
+    merge.withArgs({org: account}).returns(requestOptions);
+    paginate.withArgs(requestOptions).resolves({data: repos});
+
+    assert.deepEqual(await listNames(client, account), repoNames);
   });
 });
