87c61ff9a877f3965c56da5de269e60879cc9d8b
Matt Travi <programmer@travi.org>
2019-02-12 20:58:08 -0600

    fix(whitespace): tightened up the task definitions

--- src/repos/determine-js-projects.js
+++ src/repos/determine-js-projects.js
@@ -4,18 +4,9 @@ import fetchTravisConfigFiles from './fetch-travis-config-files';
 
 export default function (octokit, account) {
   const tasks = new Listr([
-    {
-      title: `Determining list of repositories for ${account}`,
-      task: listRepoNames
-    },
-    {
-      title: 'Fetching Travis-CI config files for each repository',
-      task: fetchTravisConfigFiles
-    },
-    {
-      title: 'Filtering to JavaScript projects',
-      task: determineJsProjects
-    }
+    {title: `Determining list of repositories for ${account}`, task: listRepoNames},
+    {title: 'Fetching Travis-CI config files for each repository', task: fetchTravisConfigFiles},
+    {title: 'Filtering to JavaScript projects', task: determineJsProjects}
   ]);
 
   return tasks.run({octokit, account, travisConfigs: {}});
