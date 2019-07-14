6e838fe0af1bbf1921904c7a802ffa2e59aca258
Matt Travi <programmer@travi.org>
2019-07-14 00:13:47 -0500

    wip(travis-client): generated and passed to the token setting process

--- src/update.js
+++ src/update.js
@@ -1,4 +1,5 @@
-import {factory as generateClient} from './github/client-factory';
+import {factory as generateGithubClient} from './github/client-factory';
+import generateTravisClient from './travis-ci/client-factory';
 import {choose} from './account';
 import listJavaScriptRepoNames from './github/determine-js-projects';
 import chooseReposFromList from './github/choose-from-list';
@@ -6,14 +7,20 @@ import setToken from './github/set-token';
 import netrc from '../third-party-wrappers/netrc';
 
 export default async function ({githubAccount} = {}) {
-  const octokit = generateClient(netrc()['github.com'].login);
+  const githubAccessToken = netrc()['github.com'].login;
+  const octokit = generateGithubClient(githubAccessToken);
 
   try {
     const account = githubAccount || await choose(octokit);
     const {jsProjects} = await listJavaScriptRepoNames(octokit, account);
     const chosenRepos = await chooseReposFromList(jsProjects);
 
-    await setToken(chosenRepos, account);
+    const [travisClient, proTravisClient] = await Promise.all([
+      generateTravisClient(githubAccessToken),
+      generateTravisClient(githubAccessToken, true)
+    ]);
+
+    await setToken(chosenRepos, account, travisClient, proTravisClient);
   } catch (err) {
     process.exitCode = 1;
     console.error(err);                               // eslint-disable-line no-console
