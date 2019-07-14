f28fb10654eb37563d3ff8ae7cac1a6c7cb432cf
Matt Travi <programmer@travi.org>
2019-07-13 23:51:14 -0500

    wip(travis-client): authenticated the client

--- src/travis-ci/client-factory.js
+++ src/travis-ci/client-factory.js
@@ -1,5 +1,13 @@
 import Travis from '../../third-party-wrappers/travis-ci';
 
-export default function (pro) {
-  return new Travis({version: '2.0.0', ...pro && {pro}});
+export default function (githubToken, pro) {
+  return new Promise((resolve, reject) => {
+    const client = new Travis({version: '2.0.0', ...pro && {pro}});
+
+    client.authenticate({github_token: githubToken}, error => {
+      if (error) reject(error);
+
+      resolve(client);
+    });
+  });
 }
