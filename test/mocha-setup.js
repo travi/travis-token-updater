21c0d2030d79bb12bc0585e194e6af4faddfa56f
Matt Travi <programmer@travi.org>
2019-07-13 23:51:14 -0500

    wip(travis-client): authenticated the client

--- test/mocha-setup.js
+++ test/mocha-setup.js
@@ -1,7 +1,9 @@
 import sinon from 'sinon';
 import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 
 sinon.assert.expose(chai.assert, {prefix: ''});
+chai.use(chaiAsPromised);
 
 process.on('unhandledRejection', reason => {
   throw reason;
--- test/unit/travis-ci/client-factory-test.js
+++ test/unit/travis-ci/client-factory-test.js
@@ -6,6 +6,7 @@ import generateClient from '../../../src/travis-ci/client-factory';
 
 suite('travis-ci client factory', () => {
   let sandbox;
+  const githubToken = any.string();
 
   setup(() => {
     sandbox = sinon.createSandbox();
@@ -15,23 +16,40 @@ suite('travis-ci client factory', () => {
 
   teardown(() => sandbox.restore());
 
-  test('that a client is created and gets authenticated', () => {
-    const createdClient = any.simpleObject();
+  test('that a client is created and gets authenticated', async () => {
+    const authenticate = sinon.stub();
+    const createdClient = {...any.simpleObject(), authenticate};
     travisCi.default.withArgs({version: '2.0.0'}).returns(createdClient);
+    authenticate.yields();
 
-    const client = generateClient();
+    const client = await generateClient(githubToken);
 
     assert.equal(client, createdClient);
     assert.calledWithNew(travisCi.default);
+    assert.calledWith(authenticate, {github_token: githubToken});
   });
 
-  test('that a client is created for the pro instance and gets authenticated', () => {
-    const createdClient = any.simpleObject();
+  test('that a client is created for the pro instance and gets authenticated', async () => {
+    const authenticate = sinon.stub();
+    const createdClient = {...any.simpleObject(), authenticate};
     travisCi.default.withArgs({version: '2.0.0', pro: true}).returns(createdClient);
+    authenticate.yields();
 
-    const client = generateClient(true);
+    const client = await generateClient(githubToken, true);
 
     assert.equal(client, createdClient);
     assert.calledWithNew(travisCi.default);
+    assert.calledWith(authenticate, {github_token: githubToken});
+  });
+
+  test('that the error is thrown if authentication fails', () => {
+    const message = any.string();
+    const error = new Error(message);
+    const authenticate = sinon.stub();
+    const createdClient = {...any.simpleObject(), authenticate};
+    travisCi.default.withArgs({version: '2.0.0'}).returns(createdClient);
+    authenticate.yields(error);
+
+    return assert.isRejected(generateClient(githubToken), error, message);
   });
 });
